80b15b2f1d070664842df44211a20a0c4a375121
00bn <nahdib0@gmail.com>
2023-08-07 21:54:18 +0400

    mode hashs

--- src/commands/ModeCommand.cpp
+++ src/commands/ModeCommand.cpp
@@ -111,11 +111,18 @@ void ModeCommand::TopicMode(ICommands* base, const int& clientSocket, IRC::Serve
 			5) send response to all clients in the channel
 
 	*/
-	Channel *existingChannel = server->serverChannelsMap[base->getParameters(command)[0]];
+
+	//------------------------------------------------------------//
 	std::string channelName = base->getParameters(command)[0];
+	Channel* existingChannel = NULL;
+	if (channelName[0] == '#'){
+		 channelName = channelName.substr(1);}
 	std::string mode = base->getParameters(command)[1];
 	const char modeChar = mode[1];
 	char modeSign = mode[0];
+	existingChannel = server->serverChannelsMap[channelName];
+	//------------------------------------------------------------//
+
 	if(existingChannel->isClientOperator(client))
 	{
 		if(modeSign == '-')
@@ -140,7 +147,7 @@ void ModeCommand::TopicMode(ICommands* base, const int& clientSocket, IRC::Serve
 	{
 		std::cout << "client is not operator MODE" << std::endl;
 		std::string response = ":" + server->serverClientsMap[clientSocket]->getNickName() + " " + ERR_CHANOPRIVSNEEDED + " " +
-			client->getNickName() + " " + channelName + " :You're not channel operator\r\n";
+			client->getNickName() + " #" + channelName + " :You're not channel operator\r\n";
 		sendResponse(clientSocket, response);
 	}
 }
@@ -148,13 +155,18 @@ void ModeCommand::TopicMode(ICommands* base, const int& clientSocket, IRC::Serve
 void ModeCommand::executeCommand(ICommands* base, const int& clientSocket, IRC::Server* server, Client* client, const std::string& command) {
 	if (!noErrorsExist(base, clientSocket, server, client, command))
 		return ;
-	std::string mode = "";
+	// std::cout << "------------------------MODE-----------------------"<< std::endl;
+	// std::cout << "mode: " << base->getParameters(command)[0] << std::endl;
+	// std::cout << "mode: " << base->getParameters(command)[1] << std::endl;
+	std::string mode ;
 	if (!base->getParameters(command).empty()) 
-		mode = base->getParameters(command)[1];
-	
+		 mode = base->getParameters(command)[1];
+	std::cout << "====>> mode: " << mode << std::endl;
 	if (mode == "+t" || mode == "-t")
 		TopicMode(base, clientSocket, server, client, command);
 	//i
+	if (mode == "+i" || mode == "-i")
+		TopicMode(base, clientSocket, server, client, command);
 	//l
 	//k
 	//o
--- src/commands/TopicCommand.cpp
+++ src/commands/TopicCommand.cpp
@@ -36,13 +36,13 @@ void TopicCommand::executeCommand(ICommands *base, const int &clientSocket, IRC:
 		else if(itChannel == server->serverChannelsMap.end())
 		{
 			std::string response = ":" + server->serverClientsMap[clientSocket]->getNickName() + " " + ERR_NOTONCHANNEL + " " +
-			 client->getNickName() + " " + channelname + " :You're not on that channel\r\n";
+			 client->getNickName() + " #" + channelname + " :You're not on that channel\r\n";
 			sendResponse(clientSocket, response);
 		}
 		else if (base->getParameters(command)[1].length() > 200)
 		{
 			std::string response = ":" + server->serverClientsMap[clientSocket]->getNickName() + " " +  ERR_NOTONCHANNEL + " " +
-			 client->getNickName() + " " + channelname + " :Topic is too long\r\n";
+			 client->getNickName() + " #" + channelname + " :Topic is too long\r\n";
 			sendResponse(clientSocket, response);
  		}
 		else if (!existingChannel->isTopicLocked() || existingChannel->isClientOperator(client))//OR AN OPERATOR 
@@ -56,7 +56,7 @@ void TopicCommand::executeCommand(ICommands *base, const int &clientSocket, IRC:
 		{
 			// NO PERMISSION
 			std::string response = ":" + server->serverClientsMap[clientSocket]->getNickName() + " " + ERR_CHANOPRIVSNEEDED + " " +
-			 client->getNickName() + " " + channelname + " :You're not channel operator\r\n";
+			 client->getNickName() + " #" + channelname + " :You're not channel operator\r\n";
 			sendResponse(clientSocket, response);
 		}
 	}
@@ -72,14 +72,14 @@ void TopicCommand::executeCommand(ICommands *base, const int &clientSocket, IRC:
 		// no topic 
 		else if (existingChannel->getTopic().empty())
 		{
- 			std::string response = ":" + server->serverClientsMap[clientSocket]->getNickName() + " TOPIC " + channelname + " :" + existingChannel->getTopic() + "\r\n";
+ 			std::string response = ":" + server->serverClientsMap[clientSocket]->getNickName() + " TOPIC #" + channelname + " :" + existingChannel->getTopic() + "\r\n";
 			std::cout << "no topic" << std::endl;
 			sendResponse(clientSocket, response);
  
 		}
 		else
 		{
- 			std::string response = ":" + server->serverClientsMap[clientSocket]->getNickName() + " TOPIC " + channelname + " :" + existingChannel->getTopic() + "\r\n";
+ 			std::string response = ":" + server->serverClientsMap[clientSocket]->getNickName() + " TOPIC #" + channelname + " :" + existingChannel->getTopic() + "\r\n";
 			std::cout << "get the topic " << existingChannel << std::endl;
 			sendResponse(clientSocket, response);
 		}
@@ -87,7 +87,7 @@ void TopicCommand::executeCommand(ICommands *base, const int &clientSocket, IRC:
 	else
 	{
 		std::string response = ":" + server->serverClientsMap[clientSocket]->getNickName() + " " + ERR_NEEDMOREPARAMS + " " +
-		 client->getNickName() + " " + channelname + " :Not enough parameters\r\n";
+		 client->getNickName() + " #" + channelname + " :Not enough parameters\r\n";
 		sendResponse(clientSocket, response);
 	}
  }
